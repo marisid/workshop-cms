acdbdd4426055ccb93c6f8e7012260854ef3cfe7
Shireen Zoaby <shireen.zoaby@gmail.com>
2016-11-07 17:59:39 +0000

    run server for girls public folder

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// var http = require('http');
-// var handler = require('./src/handler.js');
-//
-// var server = http.createServer(handler);
-//
-// server.listen(3000, function(){
-//   console.log("Server is listening to port 3000, ready to accept requests!");
-// });
+var http = require('http');
+var handler = require('./src/handler.js');
+
+var server = http.createServer(handler);
+
+server.listen(3000, function(){
+  console.log("Server is listening to port 3000, ready to accept requests!");
+});
--- src/handler.js
+++ src/handler.js
@@ -1,32 +1,32 @@
-// module.exports = handler;
-//
-// var message = 'I am so happy to be part of the Node Girls workshop!';
-// var querystring = require('querystring');
-// var defaultResponse = require('./defaultPage.js');
-//
-// function handler(request, response){
-//     var endpoint = request.url;
-//     console.log(endpoint);
-//     var extension = endpoint.split('.')[1];
-//     console.log(extension);
-//     var method = request.method;
-//     console.log(method);
-//     if (endpoint === '/') {
-//       defaultResponse(response,"/index.html","html");
-//     }
-//     else if(endpoint === '/create-post'){
-//         var allTheData = '';
-//         request.on('data', function(chunkOfData){
-//           allTheData += chunkOfData;
-//         });
-//         request.on('end',function(){
-//           var convertedData = querystring.parse(allTheData);
-//           console.log(convertedData);
-//           response.writeHead(302, {'Location': '/index.html'});
-//           response.end();
-//         });
-//     }
-//     else {
-//         defaultResponse(response,endpoint,extension);
-//       }
-// }
+module.exports = handler;
+
+var message = 'I am so happy to be part of the Node Girls workshop!';
+var querystring = require('querystring');
+var defaultResponse = require('./defaultPage.js');
+
+function handler(request, response){
+    var endpoint = request.url;
+    console.log(endpoint);
+    var extension = endpoint.split('.')[1];
+    console.log(extension);
+    var method = request.method;
+    console.log(method);
+    if (endpoint === '/') {
+      defaultResponse(response,"/index.html","html");
+    }
+    else if(endpoint === '/create-post'){
+        var allTheData = '';
+        request.on('data', function(chunkOfData){
+          allTheData += chunkOfData;
+        });
+        request.on('end',function(){
+          var convertedData = querystring.parse(allTheData);
+          console.log(convertedData);
+          response.writeHead(302, {'Location': '/index.html'});
+          response.end();
+        });
+    }
+    else {
+        defaultResponse(response,endpoint,extension);
+      }
+}
